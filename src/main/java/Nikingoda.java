e51137cc92d2ca1de2063142b953e113c240646d
nikingoda <ngoducanh6a01@gmail.com>
2025-01-23 23:35:27 +0800

    add Echo

--- src/main/java/Nikingoda.java
+++ src/main/java/Nikingoda.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Nikingoda {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,15 +10,33 @@ public class Nikingoda {
         System.out.println("Hello from\n" + logo);
         Nikingoda nikingoda = new Nikingoda();
         nikingoda.greet();
+        nikingoda.echo();
         nikingoda.exit();
     }
 
     public void greet() {
-        System.out.println("Hello! I'm nikingoda\n" +
-                "What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("\t" + "Hello! I'm nikingoda\n" +
+                "\t" + "What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void echo() {
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        if (command.equals("bye")) {
+            this.exit();
+            return;
+        }
+        System.out.println("____________________________________________________________\n" +
+                command + "\n" +
+                "____________________________________________________________");
+        this.echo();
     }
 
     public void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
